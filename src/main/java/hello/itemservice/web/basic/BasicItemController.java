be99411fc35d7477f8eb69948be31a2a2ecb1ee6
HyoungUkJJang <guddnr1451@gmail.com>
2021-05-26 21:40:56 +0900

    간단한 등록, 조회, 수정 기능 맛보기 및 리다이렉트
    - 확실히 모르고 사용했을 때보다 알아가는게 많은것 같다.

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -32,12 +31,66 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+//    public String save(@ModelAttribute("item") Item item) {
+//        itemRepository.save(item);
+//        //model.addAttribute("item", item); @modelAttribute 에 이름을 붙이면 자동으로 저장된다.. 대박
+//        //return "redirect:";
+//
+//         return "basic/item";
+//    }
+
+    /**
+     * 사실상 다 생략해도 클래스명 앞글자만 소문자로 바꿔서 모델에 자동 등록된다.
+     * @param item
+     * @return
+     */
+    @PostMapping("/add")
+        public String save(Item item) {
+            itemRepository.save(item);
+            return "basic/item";
+        }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+
+//        Item findItem = itemRepository.findById(itemId);
+//        findItem.setItemName(item.getItemName());
+//        findItem.setPrice(item.getPrice());
+//        findItem.setQuantity(item.getQuantity());
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+
+    }
+
+
+
     // 테스트용 데이터 추가
     @PostConstruct
-    public void init(){
-        itemRepository.save(new Item("itemA",10000,10));
-        itemRepository.save(new Item("itemB",145000,17));
-        itemRepository.save(new Item("itemC",34000,12));
+    public void init() {
+
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 145000, 17));
+        itemRepository.save(new Item("itemC", 34000, 12));
 
     }
 
